797c3533759072e1b9d3db29cbbbe2748da1f8dc
mannuccimattia <mannuccimattia@gmail.com>
2025-03-24 15:39:19 +0100

    [UPD] Done 03_filtra_iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function checkInitials(array, letter){
+  const initials = [];
+  for(i=0; i<array.length; i++){
+    if(array[i][0] === letter){
+      initials.push(array[i]);
+    }
+  }
+
+  return initials;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let initial = prompt("Inserisci una lettera.").toUpperCase();
+let filteredNames = checkInitials(names, initial);
+console.log(filteredNames);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
