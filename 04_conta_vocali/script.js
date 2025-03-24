c24980b1f2ba5bd3f712480fe9752fdec56b9f8a
mannuccimattia <mannuccimattia@gmail.com>
2025-03-24 15:53:13 +0100

    [UPD] Done 04_conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function checkVowels(string){
+  let vowelNum = 0;
+  let vowels = ["a", "e", "i", "o", "u"];
+  for(i=0; i<string.length; i++){
+    for(j=0; j<vowels.length; j++){
+      if(string[i].toLowerCase() === vowels[j]){
+        vowelNum += 1;
+      }
+    }
+  }
+
+  return vowelNum;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let vowelNumber = checkVowels(word);
+console.log(vowelNumber);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
