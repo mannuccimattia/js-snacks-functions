5374659fcbb04c873757488dbb4bdff381f3aae2
mannuccimattia <mannuccimattia@gmail.com>
2025-03-24 15:15:07 +0100

    [UPD] Done 01_saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,13 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function helloUser(user){
+  return `Ciao ${user}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+let welcomeUser = helloUser(userName);
+console.log(welcomeUser);
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
